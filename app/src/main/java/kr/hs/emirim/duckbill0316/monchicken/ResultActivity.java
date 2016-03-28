3bcfb3a1d362ecd9ef19c35c7f5032564ff46744
Sunggyeong Kim <skim7902@gmail.com>
2016-03-28 20:14:44 +0900

    #2 add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/duckbill0316/monchicken/ResultActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.duckbill0316.monchicken;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by appcreator01 on 2016. 3. 28..
+ */
+public class ResultActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
