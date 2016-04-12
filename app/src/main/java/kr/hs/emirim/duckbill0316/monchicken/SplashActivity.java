327ae840d2fa3ac814fac139da65262e12bab04c
Sunggyeong Kim <skim7902@gmail.com>
2016-04-12 19:20:42 +0900

    #9 add animation & zoom_exit

--- app/src/main/java/kr/hs/emirim/duckbill0316/monchicken/SplashActivity.java
+++ app/src/main/java/kr/hs/emirim/duckbill0316/monchicken/SplashActivity.java
@@ -15,14 +15,15 @@ public class SplashActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_splash);
 
-        //지정 시간에 따라 화면이 넘어간다!
+        //지정 시간에 따라 화면이 넘어간다! (일정 시간 지정 가능!)
         Handler handler=new Handler();
 
         handler.postDelayed(new Runnable() {  //익명 객
             @Override
-            public void run() {
+            public void run() { //(객체의 잠조 값 , ms단위의 시간 값)
                 Intent intent=new Intent(getApplicationContext(),MainActivity.class); //액티비티 시작시 필요한 객체 (인턴트 객체)
                 startActivity(intent);
+                overridePendingTransition(0,R.anim.zoom_exit);
                 finish(); //현재 액티비트를 닫아줌
             }
         },3000); //1초에 1000 (ms 단위)
